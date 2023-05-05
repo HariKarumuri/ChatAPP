ee6906b35fe6c68da5c95ea5a835ffa7bb1e6377
hari <harikarumuri1086@gmail.com>
2023-05-05 18:43:47 +0530

    done changes

--- src/Chat.js
+++ src/Chat.js
@@ -4,6 +4,7 @@ import ScrollToBottom from "react-scroll-to-bottom";
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
+  const [showWarning, setShowWarning] = useState(false);
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -22,8 +23,33 @@ function Chat({ socket, username, room }) {
       setCurrentMessage("");
     }
   };
+  const handleInputChange = (event) => {
+    setCurrentMessage(event.target.value);
+    setShowWarning(false); // hide warning when input changes
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      const messageText = event.target.value.trim();
+      if (messageText === "") return;
+
+      // Check if message contains hate words
+      const hateWords = ["hate", "discriminate", "bigotry"];
+      if (hateWords.some((word) => messageText.includes(word))) {
+        setShowWarning(true);
+        // You can also send a message to the server to ban the user with the socket.emit() function
+      } else {
+        setShowWarning(false);
+        sendMessage();
+      }
+    }
+  };
 
   useEffect(() => {
+    // Remove the existing event listener before adding a new one
+    socket.off("receive_message");
+  
+    // Add the event listener
     socket.on("receive_message", (data) => {
       setMessageList((list) => [...list, data]);
     });
@@ -31,6 +57,9 @@ function Chat({ socket, username, room }) {
 
   return (
     <div className="chat-window">
+      {showWarning && (
+        <div className="warning-message">Hate word detected. Please be respectful.</div>
+      )}
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
@@ -61,12 +90,9 @@ function Chat({ socket, username, room }) {
           type="text"
           value={currentMessage}
           placeholder="Hey..."
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
-          onKeyPress={(event) => {
-            event.key === "Enter" && sendMessage();
-          }}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
+          
         />
         <button onClick={sendMessage}>&#9658;</button>
       </div>
